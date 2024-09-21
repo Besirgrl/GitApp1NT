d52f49edffa84192451b19331c1e38d90b7c20f2
Beşir Gürel <brownbesir@gmail.com>
2024-09-21 14:20:07 +0200

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,7 @@ public class Runner {
     public static void main(String[] args) {
 
         System.out.println("version 1");
+        System.out.println("version 2");
+        System.out.println("continuation of version 2");
     }
 }
